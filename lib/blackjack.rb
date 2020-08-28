9ded26b6a83d7187ebcc0563a8d1223dbf3f8a69
Brian Feldman <brian.feldman42@gmail.com>
2020-08-28 19:38:04 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -71,7 +71,8 @@ def runner
   card_total = initial_round
   hit?(card_total)
   until card_total > 21
-    hit?(card_total)
+    card_total = hit?(card_total)
+    
   end
   end_game(card_total)
 end
